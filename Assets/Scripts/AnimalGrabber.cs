22e199677949558fbff84f34139ab41084e625c6
JoJo777890 <JoJo777890@gmail.com>
2024-02-28 08:36:01 +0800

    2024-2-28 (Milestone) Implement Correct Animal Will Appear to "BookSymbol"
    (Milestone) Implement Correct Animal Will Appear to "BookSymbol".

--- Assets/Scripts/AnimalGrabber.cs
+++ Assets/Scripts/AnimalGrabber.cs
@@ -6,17 +6,34 @@ using UnityEngine;
 
 public class AnimalGrabber : MonoBehaviour
 {
+    private GameObject[] animals;
     private Scanner scanner;
     private string resultWord;
     private void Start()
     {
         scanner = GetComponent<Scanner>();
-        scanner.onFetchedResultWord += fetchResultWord;
+        scanner.onFetchedResultWord += FetchResultWord;
     }
 
-    void fetchResultWord()
+    void FetchResultWord()
     {
         this.resultWord = scanner.resultWord;
         Debug.Log("New Single Result Word: " + resultWord);
+        
+        Invoke("MoveAnimalToBookSymbol", 0.5f);
+    }
+
+    void MoveAnimalToBookSymbol()
+    {
+        animals = GameObject.FindGameObjectsWithTag("Animals");
+
+        foreach (GameObject animal in animals)
+        {
+            if (string.Equals(animal.name, resultWord))
+            {
+                animal.transform.SetParent(transform.parent, true);
+                animal.transform.position = transform.parent.position;
+            }
+        }
     }
 }
