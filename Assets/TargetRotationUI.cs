1ef24e6a6b52d9537c1ae61fc10d6b9048096d00
JoJo777890 <JoJo777890@gmail.com>
2025-06-16 21:59:42 +0800

    2025-6-16 Created "Reboot_PitchDemo" For Professor
    Relative Position_A&C,
    Rotation_A,
    Rotation_C

---
+++ Assets/TargetRotationUI.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TargetRotationUI : MonoBehaviour
+{
+    public GameObject imageTargetA;
+    public GameObject imageTargetC;
+
+    public TextMeshProUGUI infoTextA;
+    public TextMeshProUGUI infoTextC;
+
+    void Update()
+    {
+        if (imageTargetA.activeInHierarchy)
+        {
+            Vector3 rotA = imageTargetA.transform.eulerAngles;
+
+            infoTextA.text = $"[Target A]\nRot: X: {360 - rotA.x:F1}\nRot: Y: {360 - rotA.z:F1}\nRot: Z: {rotA.y:F1}";
+        }
+
+        if (imageTargetC.activeInHierarchy)
+        {
+            Vector3 rotC = imageTargetC.transform.eulerAngles;
+
+            infoTextC.text = $"[Target C]\nRot: X: {360 - rotC.x:F1}\nRot: Y: {360 - rotC.z:F1}\nRot: Z: {rotC.y:F1}";
+        }
+    }
+}
