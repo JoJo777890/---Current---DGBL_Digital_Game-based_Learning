ca838b07e22f5b8c210a7f698a4e342706a9014a
JoJo777890 <JoJo777890@gmail.com>
2025-07-08 17:50:38 +0800

    2025-7-8 Added more comments in "ARCrosswordScanner.cs"

--- Assets/ARCrosswordScanner.cs
+++ Assets/ARCrosswordScanner.cs
@@ -12,6 +12,12 @@ public class AutoARCrosswordScanner : MonoBehaviour
 
     private List<TrackedLetter> trackedLetters = new List<TrackedLetter>();
 
+    class TrackedLetter
+    {
+        public char letter;
+        public Vector3 position;
+    }
+
     void Update()
     {
         trackedLetters.Clear();
@@ -45,6 +51,7 @@ public class AutoARCrosswordScanner : MonoBehaviour
 
     string ScanRows(List<TrackedLetter> letters)
     {
+        // The List Structure is --> "rows" of "row" of "TrackedLetter".
         List<List<TrackedLetter>> rows = new List<List<TrackedLetter>>();
 
         foreach (var letter in letters)
@@ -52,6 +59,7 @@ public class AutoARCrosswordScanner : MonoBehaviour
             bool added = false;
             foreach (var row in rows)
             {
+                // Check if this letter has the same height as an existing row.
                 if (Mathf.Abs(letter.position.y - row[0].position.y) < groupThreshold)
                 {
                     row.Add(letter);
@@ -60,22 +68,24 @@ public class AutoARCrosswordScanner : MonoBehaviour
                 }
             }
 
-            // Create a new row if the current row has no letters added.
+            // Create a new row, if all existing rows have no letters being added.
             if (!added)
                 rows.Add(new List<TrackedLetter> { letter });
         }
 
         // Sort rows top to bottom
         rows.Sort((a, b) => b[0].position.y.CompareTo(a[0].position.y));
+        // Sort row left to right
         foreach (var row in rows)
             row.Sort((a, b) => a.position.x.CompareTo(b.position.x));
 
+        // Create row UI Text string
         string result = "";
         foreach (var row in rows)
         {
             foreach (var l in row)
                 result += l.letter;
-            result += ", "; // Originally "\n"
+            result += ", ";
         }
 
         return result.Trim();
@@ -83,6 +93,7 @@ public class AutoARCrosswordScanner : MonoBehaviour
 
     string ScanColumns(List<TrackedLetter> letters)
     {
+        // The List Structure is --> "rows" of "row" of "TrackedLetter".
         List<List<TrackedLetter>> columns = new List<List<TrackedLetter>>();
 
         foreach (var letter in letters)
@@ -90,6 +101,7 @@ public class AutoARCrosswordScanner : MonoBehaviour
             bool added = false;
             foreach (var col in columns)
             {
+                // Check if this letter has the same height as an existing row.
                 if (Mathf.Abs(letter.position.x - col[0].position.x) < groupThreshold)
                 {
                     col.Add(letter);
@@ -97,29 +109,27 @@ public class AutoARCrosswordScanner : MonoBehaviour
                     break;
                 }
             }
+
+            // Create a new row, if all existing rows have no letters being added.
             if (!added)
                 columns.Add(new List<TrackedLetter> { letter });
         }
 
         // Sort columns left to right
         columns.Sort((a, b) => a[0].position.x.CompareTo(b[0].position.x));
+        // Sort column top to bottom
         foreach (var col in columns)
             col.Sort((a, b) => b.position.y.CompareTo(a.position.y)); // top to bottom
 
+        // Create row UI Text string
         string result = "";
         foreach (var col in columns)
         {
             foreach (var l in col)
                 result += l.letter;
-            result += ", "; // Originally "\n"
+            result += ", ";
         }
 
         return result.Trim();
-    }
-
-    class TrackedLetter
-    {
-        public char letter;
-        public Vector3 position;
     }
 }
